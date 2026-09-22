09227d25300da387701a11d564b9284c3e6c8ddb
Wellie95 <38663060+Wellie95@users.noreply.github.com>
2026-09-22 18:41:08 +0200

    Build TENDIA React printing website

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Routes,Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Pricing from "./pages/Pricing";
+import Gallery from "./pages/Gallery";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+export default function App(){return <><Navbar/><main><Routes>
+<Route path="/" element={<Home/>}/><Route path="/services" element={<Services/>}/><Route path="/pricing" element={<Pricing/>}/>
+<Route path="/gallery" element={<Gallery/>}/><Route path="/about" element={<About/>}/><Route path="/contact" element={<Contact/>}/>
+</Routes></main><Footer/></>}
